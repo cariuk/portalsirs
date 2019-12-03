0324186973be410b42e45e1bdeeb42da2caca844
hademopilie <hademopilie@gmail.com>
2019-12-03 23:27:32 +0800

    Perbaikan

--- app/Http/Controllers/Klaim/DashboardController.php
+++ app/Http/Controllers/Klaim/DashboardController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Klaim;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class DashboardController extends Controller
-{
+class DashboardController extends IndexController{
     //
 }
--- app/Http/Controllers/Klaim/IndexController.php
+++ app/Http/Controllers/Klaim/IndexController.php
@@ -5,7 +5,19 @@ namespace App\Http\Controllers\Klaim;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class IndexController extends Controller
-{
-    //
+class IndexController extends Controller{
+    public $pathview,$module,$data;
+
+    public function __construct(){
+        $this->pathview = "klaim.";
+    }
+
+    public function index(Request $request){
+        extract(get_object_vars($this));
+        $view = view($this->pathview.$this->module.'.index',compact('module','data'))->render();
+        return response()->json([
+            "status" => 200,
+            "subcontent" => $view
+        ]);
+    }
 }
